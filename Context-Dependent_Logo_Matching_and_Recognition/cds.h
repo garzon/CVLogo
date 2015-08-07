cbbb9f784528cff781fa051e5c5f06ce715b8241
LiYingwei <liyw14@163.com>
2015-08-07 15:03:14 +0800

    start from here

--- Context-Dependent_Logo_Matching_and_Recognition/cds.h
+++ Context-Dependent_Logo_Matching_and_Recognition/cds.h
@@ -2,16 +2,17 @@
 #define CDS_H
 
 //#define DEBUG
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <set>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <vector>
-#include <set>
-using namespace std;
-using namespace cv;
+
 class CDS
 {
 public:
@@ -19,32 +20,28 @@ public:
 	void setAlpha(float Alpha) { alpha = Alpha; }
 	void setBeta(float Beta) { beta = Beta; }
 	void setTau(float Tau) { tau = Tau; }
-	void setNa(float Na) { this->Na = Na; }
 	void setNr(float Nr) { this->Nr = Nr; }
 	void setEps(float Eps) { eps = Eps; }
 	void setMaxTheta(float theta) { maxtheta = theta; }
 	void setMaxRho(float rho) { maxrho = rho; }
-	void setParameters(float Alpha, float Beta, float Tau, float Na, float Nr, float eps);
 	float getAlpha() { return alpha; }
 	float getBeta() { return beta; }
 	float getTau() { return tau; }
-	float getNa() { return Na; }
 	float getNr() { return Nr; }
 	float getEps() { return eps; }
 	float getMaxTheta() { return maxtheta; }
 	float getMaxRho() { return maxrho; }
 
-	void setInputImage(Mat inputimage)
+	void setInputImage(cv::Mat inputimage)
 	{
-		//resize(inputimage,Ix,Size(200,200));
-		Ix = inputimage;
+		float cols = 500;
+		float rows = 500.0 / inputimage.cols * inputimage.rows;
+		resize(inputimage,Ix,cv::Size(cols,rows));
 	}
-	void setTestImage(Mat testimage)
+	void setTestImage(cv::Mat testimage)
 	{
-		resize(testimage,Iy,Size(500,500));
+		resize(testimage,Iy,cv::Size(500,500));
 	}
-	Mat getInputImage() { return Ix; }
-	Mat getTestImage() { return Iy; }
 
 	void extractSIFTKeyPoint();
 	void computeContext();
@@ -52,25 +49,48 @@ public:
 	bool match();
 	void visualize();
 	bool process();
-private:
-	float alpha, beta, tau, Nr, eps;
-	int maxtheta, maxrho, maxt;
-	float threshold, Na, responseThresholdx,responseThresholdy;
-	std::vector<KeyPoint> Sx, Sy;
-	Mat Ix,Iy;
-	vector< vector<Mat> > P, Q; // I think SparseMat is better, but I don't know how to use it.
-	Mat K,D;
-	Mat Kp;
-	std::set<int> matchY;
-	vector<DMatch> matchVec;
-	Mat showImg;
+	void drawRectanglePlanA();
+	void drawRectanglePlanB();
 
-	Mat G(const Mat &K) const;
-	float dist(const KeyPoint &p1, const KeyPoint &p2) const;
-	void unpackSIFTOctave(const KeyPoint& kpt, int& octave, int& layer, float& scale);
-	int unpackSIFTOctave(const KeyPoint& kpt);
+	//正式接口
+	struct Param
+	{
+		float alpha, beta, tau, Nr, eps, eeps;
+		int maxtheta, maxrho, maxt;
+		float threshold, responseThresholdx,responseThresholdy, rectangleThreshold;
+	}param;
+	void setParameters(const Param& param);
+	Param getParameters(){return param;}
+	void writeParameters(std::ofstream &outfile);
+	void readParameters(std::ifstream &infile);
+	void writeLogo(std::ofstream &outfile);
+	void getSiftKeyPoint(std::vector<cv::KeyPoint> &Sx, std::vector<cv::KeyPoint> &Sy);
+	cv::Mat getInputImage() { return Ix; }
+	cv::Mat getTestImage() { return Iy; }
+	std::vector<cv::DMatch> getMatchVec() { return matchVec;}
+private:
+	//参数
+	float alpha, beta, tau, Nr, eps, eeps;
+	int maxtheta, maxrho, maxt;
+	float threshold, responseThresholdx,responseThresholdy, rectangleThreshold;
+	//result
+	std::vector<cv::KeyPoint> Sx, Sy;
+	cv::Mat Dx,Dy;//存放特征向量的矩阵
+	cv::Mat Ix,Iy;
+	std::vector<std::vector<cv::Mat> > P, Q; // I think SparseMat is better, but I don't know how to use it.
+	cv::Mat K,D;
+	cv::Mat Kp;
+	std::vector<cv::DMatch> matchVec;
+	//private
+	std::set<int> matchY;
+	cv::Mat showImg;
+
+	cv::Mat G(const cv::Mat &K) const;
+	float dist(const cv::KeyPoint &p1, const cv::KeyPoint &p2) const;
+	void unpackSIFTOctave(const cv::KeyPoint& kpt, int& octave, int& layer, float& scale);
+	int unpackSIFTOctave(const cv::KeyPoint& kpt);
 	static void callBackFuncForROI(int event, int x, int y, int flags, void* userdata);
-	static Point2f ROIpt1, ROIpt2;
+	static cv::Point2f ROIpt1, ROIpt2;
 };
 
 #endif // CDS_H
