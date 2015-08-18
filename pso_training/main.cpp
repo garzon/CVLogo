beb31adcf444fea69a80ca3475a61bb3b4d1c5b4
Jiajun Ou <garzonou@gmail.com>
2015-08-18 10:43:43 +0800

    add parallel for

--- pso_training/main.cpp
+++ pso_training/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     vector<string> trainingSet = { "/home/garzon/1.jpg", "/home/garzon/2.jpg", "/home/garzon/3.jpg", "/home/garzon/4.jpg" };
     SiftCost_1 func1(trainingSet, "/home/garzon/logo.jpg");
-	Pso<SiftCost> pso(func1);
+    Pso<SiftCost_1> pso(func1);
 	SiftParams best_params = pso.solve();
 	cout << "cost: " << func1.costFunction(best_params) << endl;
 	cout << best_params.toMat() << endl;
