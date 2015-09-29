95d84fa18082fa1dcbaacc2a9eed8b3253b86773
ÂÇ®‰ª≤Â∞ß <czy8023@me.com>
2015-09-29 19:37:11 +0800

    Initial Commit

---
+++ CVLogo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CVLogo
+//
+//  Created by 储仲尧 on 9/29/15.
+//  Copyright © 2015 FUDAN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
