3fa731ac8eb3da3657e90747a5feb65a91858d25
储仲尧 <czy8023@me.com>
2015-10-09 21:35:00 +0800

    no message

--- CVLogo/main.cpp
+++ CVLogo/main.cpp
@@ -159,7 +159,34 @@ int main(int argc, char *argv[])
 using namespace std;
 using namespace cv;
 int main(int argvs,char* argv[]){
-    cout<<"Hello Charming!"<<endl;
-    cout<<curl_version()<<endl;
+    CURL *curlHandle=curl_easy_init();
+    char* url;
+    url="http://localhost/CVLogo/generator.php";
+    CURL *curl;
+    CURLcode res;
+    /* In windows, this will init the winsock stuff */
+    curl_global_init(CURL_GLOBAL_ALL);
+    
+    /* get a curl handle */
+    curl = curl_easy_init();
+    if(curl) {
+        /* First set the URL that is about to receive our POST. This URL can
+         just as well be a https:// URL if that is what should receive the
+         data. */
+        curl_easy_setopt(curl, CURLOPT_URL,url);
+        /* Now specify the POST data */
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+        
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        cout<<endl;
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
     return 0;
 }
